f3c671069cbe5ab1352a40c91167521c52a65fd8
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-08-13 21:12:54 -0400

    completed lab. most difficult thus far.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+}
+
+function updateEmployeeWithKeyAndValue(){
+    const updateEmployee = {
+        ...employee,
+        streetAddress: "11 Broadway",
+    };
+    return updateEmployee;
+}
+
+const updateEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "streetAddress",
+    "11 Broadway",
+)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(updateEmployee){
+    const deleteEmployee = {...employee}
+    delete deleteEmployee.name;
+    return deleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(newEmployee){
+    delete employee.name;
+    return employee;
+}
